0f677c2ec6ecd1ca224490e907fc247768e37f43
Vini Poffo <vini@vinipoffo.com>
2026-01-28 17:22:59 -0500

    Fix: Revert reversible carousel, reorder projects

--- public/data.js
+++ public/data.js
@@ -9,6 +9,17 @@ const siteData = {
   },
   projects: [
     {
+      id: 3,
+      title: "Tem feito uns dias esquisitos",
+      year: 2025,
+      type: "curta-metragem",
+      artist: "Web série",
+      videoHome: "tfde_portfolio_video.mp4",
+      cartazMobile: "dias-esquisitos-cartaz.png",
+      trailerUrl: "https://youtu.be/VoZ8e76yASQ?si=T5BxmH5B-QIwLYRr",
+      description: "Primeira web série de Vini Poffo. Direção, roteiro e concepção de arte. Premiada pela Mostra SESC de Cinema 2025 e Prêmio Catarinense de Cinema."
+    },
+    {
       id: 1,
       title: "Vira essa boca pra cá",
       year: 2025,
@@ -31,17 +42,6 @@ const siteData = {
       description: "Videoclipe de Dropar teu nome, dirigido por Vini Poffo. Lançamento pela Noize."
     },
     {
-      id: 3,
-      title: "Tem feito uns dias esquisitos",
-      year: 2025,
-      type: "curta-metragem",
-      artist: "Web série",
-      videoHome: "tfde_portfolio_video.mp4",
-      cartazMobile: "dias-esquisitos-cartaz.png",
-      trailerUrl: "https://youtu.be/VoZ8e76yASQ?si=T5BxmH5B-QIwLYRr",
-      description: "Primeira web série de Vini Poffo. Direção, roteiro e concepção de arte. Premiada pela Mostra SESC de Cinema 2025 e Prêmio Catarinense de Cinema."
-    },
-    {
       id: 4,
       title: "Outres de Nós",
       year: 2025,
@@ -51,6 +51,17 @@ const siteData = {
       cartazMobile: "outres-nos-cartaz.png",
       trailerUrl: "https://youtu.be/w2uoe7uvmQQ?si=4vmcp7Dq3mBHq9o_",
       description: "Videoclipe de Outres de Nós, dirigido por Vini Poffo."
+    },
+    {
+      id: 7,
+      title: "No reflexo do meu nome",
+      year: 2023,
+      type: "curta-metragem",
+      artist: "Curta-metragem",
+      videoHome: "noreflexo_portfolio_video.mp4",
+      cartazMobile: "reflexo-nome-cartaz.png",
+      trailerUrl: "https://vimeo.com/820329509?fl=pl&fe=sh",
+      description: "Curta-metragem No Reflexo do Meu Nome, direção e roteiro de Vini Poffo. Eleito pelo SESC SC em 2022 como melhor obra de Santa Catarina. Selecionado para mostra SESC de cinema nacional 2023, primeiro filme de SC a entrar no circuito. Exibido em mais de 15 festivais nacionais e internacionais."
     },
     {
       id: 5,
@@ -73,17 +84,6 @@ const siteData = {
       cartazMobile: "suburbana-cartaz.png",
       trailerUrl: "https://vimeo.com/820330742?fl=pl&fe=sh",
       description: "Curta-metragem (Sub)Urbana, direção e co-roteiro de Vini Poffo. Conquistou 5 prêmios de melhor filme, incluindo Prêmio Revelação no IV Transforma Festival. Percorreu mais de 20 festivais nacionais e internacionais. Disponível no Itaú Play."
-    },
-    {
-      id: 7,
-      title: "No reflexo do meu nome",
-      year: 2023,
-      type: "curta-metragem",
-      artist: "Curta-metragem",
-      videoHome: "noreflexo_portfolio_video.mp4",
-      cartazMobile: "reflexo-nome-cartaz.png",
-      trailerUrl: "https://vimeo.com/820329509?fl=pl&fe=sh",
-      description: "Curta-metragem No Reflexo do Meu Nome, direção e roteiro de Vini Poffo. Eleito pelo SESC SC em 2022 como melhor obra de Santa Catarina. Selecionado para mostra SESC de cinema nacional 2023, primeiro filme de SC a entrar no circuito. Exibido em mais de 15 festivais nacionais e internacionais."
     },
     {
       id: 8,
